bb7fa113d2b8e13fe24a774873bde100c32a0188
Aradhya ks <ksarya6196@gmail.com>
2026-03-22 11:56:47 +0530

    Revise Twilio account details and recipient numbers
    Updated Twilio credentials and phone numbers for messaging.

--- sms.py
+++ sms.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = "AC600926e628bfb15031238b623b4e1ae7"
-auth_token = "9313988d78359cb1a192572c53523b4f"
+account_sid = "Account _sid"
+auth_token = "YOUR_AUTH_TOKEN"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     body="Test message from aid tracking app ✅",
-    from_="+12294598732",      # Your Twilio number
-    to="+918197437307"         # Verified number
+    from_="+123....",      # Your Twilio number
+    to="+1234567890"         # Verified number
 )
 
 print("Message SID:", message.sid)
